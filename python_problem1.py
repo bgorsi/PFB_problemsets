8358e4b5fb110daad209ca3824bb6f0dc572d553
bgo <bgo@pfb04.cshl.edu>
2018-10-29 11:20:25 -0400

    add py scripts

---
+++ python_problem1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+name = 'Bushra'
+color = 'purple'
+activity = 'coding'
+animal = 'zebra'
+print("My name is", name)
+print("My favorite color is", color)
+print("My favorite activity is", activity)
+print("My favorite animal is", animal)
+
+print("end of script")
+
